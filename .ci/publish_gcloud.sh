8ef8992c1a8e279025b57c4344dae68fc9ee40e8
Robin de Rooij <rderooij685@gmail.com>
2021-03-08 17:03:17 +0100

    ci: try to adjust for new hub desktop

--- .ci/publish_gcloud.sh
+++ .ci/publish_gcloud.sh
@@ -1,2 +1,2 @@
 BRANCH=$1
-gsutil cp -r console/dist/* "gs://cdn.terminusdb.com/js_libs/terminusdb_console/${BRANCH}/"
+gsutil cp -r console/dist/* "gs://cdn.terminusdb.com/js_libs/terminusdb_hub_console/${BRANCH}/"
